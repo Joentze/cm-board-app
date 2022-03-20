dc6e714631ce17f441cfcfca162502cebba08e9b
Joentze <Joentze01@gmail.com>
2022-03-20 18:52:51 +0800

    commit

---
+++ src/components/NoAccess.js
@@ -0,0 +1,8 @@
+import purple_pending_approval from "../assets/svg/purple_pending_approval.svg";
+export const NoAccess = () => {
+  return (
+    <div>
+      <img src={purple_pending_approval} />
+    </div>
+  );
+};
